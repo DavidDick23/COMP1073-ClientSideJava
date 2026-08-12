1a96468d4ded802a78407c6e158e18970ad4f10d
DavidDick23 <david.dck@gmail.com>
2026-08-11 20:56:56 -0400

    Selected NASA picture of the day, obtained API key, completed HTML, roughed in styles and dynamically added student name/id to footer

---
+++ assignment-3/js/assignment-3.js
@@ -0,0 +1,3 @@
+// Displays my name and student id in the p element with the id of "student-info"
+const studentInfo = document.querySelector("#student-info");
+studentInfo.textContent = "©2026 | David Dick - 100099683"; // alt-0169 for copywrite symbol found at https://www.alt-codes.net/copyright_alt_code.php
